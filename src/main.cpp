763724e803bab1fb5bfc3b973e2ce52db28ce34d
Waaait <wli2@mta.ca>
2022-04-04 14:27:28 -0300

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@
 #include "graphics/rendering/texture.h"
 #include "graphics/models/cube.hpp"
 #include "graphics/models/lamp.hpp"
-//#include "graphics/models/item.hpp"
 #include "graphics/objects/model.h"
 
 #include "io/keyboard.h"
@@ -74,7 +73,6 @@ int main() {
 	glEnable(GL_DEPTH_TEST);
 
 	// SHADERS===============================
-	//Shader shader("assets/vertex_core.glsl", "assets/fragment_core.glsl");
 	Shader shader("assets/object.vs", "assets/object.fs");
 	Shader lampShader("assets/object.vs", "assets/lamp.fs");
 
@@ -82,39 +80,14 @@ int main() {
 	Cube model(Material::red_plastic, glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.75f));
 	model.init();
 
-	
+	// LIGHTS
+
 	lightPosX = -1.0f;
 	lightPosY = -.5f;
 	lightPosZ = .5f;
 
 	Lamp lamp(glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(lightPosX, lightPosY, lightPosZ), glm::vec3(0.25f));
 	lamp.init();
-
-	// LIGHTS
-	// 
-	//glm::vec3 pointLightPositions[] = {
-	//	glm::vec3(0.7f,  0.2f,  2.0f),
-	//	glm::vec3(2.3f, -3.3f, -4.0f)/*,
-	//	glm::vec3(-4.0f,  2.0f, -12.0f),
-	//	glm::vec3(0.0f,  0.0f, -3.0f)*/
-	//};
-	//Lamp lamps[4];
-	/*const int lampNum = 2;
-	Lamp lamps[lampNum];
-	for (unsigned int i = 0; i < lampNum; i++) {
-		lamps[i] = Lamp(glm::vec3(1.0f),
-			glm::vec4(0.05f, 0.05f, 0.05f, 1.0f), glm::vec4(0.8f, 0.8f, 0.8f, 1.0f), glm::vec4(1.0f),
-			1.0f, 0.07f, 0.032f,
-			pointLightPositions[i], glm::vec3(0.25f));
-		lamps[i].init();
-	}*/
-
-	/*SpotLight s = {
-		Camera::defaultCamera.cameraPos, Camera::defaultCamera.cameraFront,
-		glm::cos(glm::radians(12.5f)), glm::cos(glm::radians(20.0f)),
-		1.0f, 0.07f, 0.032f,
-		glm::vec4(0.0f, 0.0f, 0.0f, 1.0f), glm::vec4(1.0f), glm::vec4(1.0f)
-	};*/
 
 	// TEXTURES_____________________________________
 	
@@ -129,7 +102,6 @@ int main() {
 
 		// render
 		glClearColor(.0f, .0f, .0f, 0.1f);
-		//glClearColor(0.8f, 0.8f, 0.7f, 0.1f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
 		shader.activate();
@@ -144,7 +116,6 @@ int main() {
 		// create transformation
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
-		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f, 0.5f, 0.5f));
 		view = glm::translate(view, glm::vec3(-x, -y, -z));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
@@ -157,9 +128,6 @@ int main() {
 		lampShader.setMat4("view", view);
 		lampShader.setMat4("projection", projection);
 		lamp.render(lampShader,glm::vec3(lightPosX, lightPosY, lightPosZ));
-		/*for (unsigned int i = 0; i < lampNum; i++) {
-			lamps[i].render(lampShader);
-		}*/
 
 
 		// send new frame to window
@@ -169,10 +137,6 @@ int main() {
 
 	model.cleanup();
 	lamp.cleanup();
-	//cube.cleanup();
-	/*for (unsigned int i = 0; i < lampNum; i++) {
-		lamps[i].cleanup();
-	}*/
 	glfwTerminate();
 	return 0;
 }
